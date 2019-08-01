b0cdb9f0189387801c0d51d12978b856e5efe640
kylesun <457799096@qq.com>
2019-08-01 17:52:21 +0800

    new commit

---
+++ src/Algorithms/JZoffer/Question11.java
@@ -0,0 +1,20 @@
+package Algorithms.JZoffer;
+
+public class Question11 {
+    public static void main(String[] args) {
+        Question11 question11 = new Question11();
+        question11.NumberOf1(10);
+
+    }
+
+    public int NumberOf1(int n) {
+//        Java自带的函数
+//        return Integer.bitCount(n);
+        int i ;
+        for ( i=0;n!=0;i++){
+            n = n&(n-1);
+
+        }
+        return i ;
+    }
+}
