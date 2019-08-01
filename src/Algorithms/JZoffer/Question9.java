c2e7ca10f185248d9a7e53b32bc2af2b90a44c4a
kylesun <457799096@qq.com>
2019-08-01 11:00:43 +0800

    new commit

---
+++ src/Algorithms/JZoffer/Question9.java
@@ -0,0 +1,23 @@
+package Algorithms.JZoffer;
+
+public class Question9 {
+    public static void main(String[] args) {
+        Question9 question9 = new Question9();
+        System.out.println(question9.JumpFloorII(4));
+    }
+
+    public int JumpFloorII(int target) {
+         if(target==1||target==2)
+             return target;
+         int[] array = new int[target+1];
+         array[0]=1;
+         array[1]=1;
+         array[2]=2;
+         for(int i=3;i<=target;i++){
+             for(int j=0;j<i;j++){
+                 array[i]+=array[j];
+             }
+         }
+         return array[target];
+    }
+}
