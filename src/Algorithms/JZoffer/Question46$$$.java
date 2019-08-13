6b8741dc14524abac0a363a9e26ff6b96cc1195a
kylesun <457799096@qq.com>
2019-08-13 22:42:36 +0800

    new commit

---
+++ src/Algorithms/JZoffer/Question46$$$.java
@@ -0,0 +1,13 @@
+package Algorithms.JZoffer;
+//&&与递归 佛辣
+public class Question46$$$ {
+    public static void main(String[] args) {
+        Question46$$$ question46$$$ = new Question46$$$();
+        System.out.println(question46$$$.Sum_Solution(1));
+    }
+    public int Sum_Solution(int n) {
+        int temp=n;
+        boolean b = (temp>0)&&(temp+=Sum_Solution(n-1))>0;
+        return temp;
+    }
+}
---
+++ src/experiment/Polymorphic.java
@@ -0,0 +1,27 @@
+package experiment;
+
+public class Polymorphic {
+    public static void main(String[] args) {
+        Person person = new Man();
+        person.say();//i am a man!
+//        Man man = new Person(); 错误
+    }
+}
+
+class Person{
+    public void say(){
+        System.out.println("i am a person!");
+    }
+}
+class Man extends Person{
+    @Override
+    public  void say(){
+        System.out.println("i am a man!");
+    }
+}
+class Woman extends Person{
+    @Override
+    public  void say(){
+        System.out.println("i am a Woman!");
+    }
+}
