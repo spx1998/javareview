fe0417420540ee0ef17a3dab6b2074b6ec3e662e
kylesun <457799096@qq.com>
2019-08-20 10:06:08 +0800

    new commit

---
+++ src/Algorithms/datastructure/TreeLinkNode.java
@@ -0,0 +1,12 @@
+package Algorithms.datastructure;
+
+public class TreeLinkNode {
+    public int val;
+    public TreeLinkNode left = null;
+    public TreeLinkNode right = null;
+    public TreeLinkNode next = null;
+
+    public TreeLinkNode(int val) {
+        this.val = val;
+    }
+}
