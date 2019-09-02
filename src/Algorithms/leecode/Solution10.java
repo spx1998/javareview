6f82bf467c2bc754871f70b319f458dd20e41799
kylesun <457799096@qq.com>
2019-09-02 18:56:21 +0800

    new commit

---
+++ src/Algorithms/leecode/Solution10.java
@@ -0,0 +1,32 @@
+package Algorithms.leecode;
+
+public class Solution10 {
+    public static void main(String[] args) {
+        System.out.println(new Solution10().isMatch("aaa",
+                "ab*ac*a"));
+
+    }
+
+
+    public boolean isMatch(String s, String p) {
+        int ns = s.length() + 1, np = p.length() + 1;
+        boolean[][] dp = new boolean[ns][np];
+        dp[0][0] = true;
+        for (int i = 0; i < ns; i++) {
+            for (int j = 1; j < np; j++) {
+                if (i > 0 && (p.charAt(j - 1) == '.' || p.charAt(j - 1) == s.charAt(i - 1))) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                }
+                if (p.charAt(j - 1) == '*') {
+                    if (i == 0 || (s.charAt(i - 1) != p.charAt(j - 2) && p.charAt(j - 2) != '.')) {
+                        dp[i][j] = dp[i][j - 2];
+                    } else {
+                        dp[i][j] = dp[i - 1][j] || dp[i][j - 1] || dp[i][j - 2];
+                    }
+                }
+            }
+        }
+        return dp[ns - 1][np - 1];
+
+    }
+}
---
+++ src/Algorithms/leecode/Solution5.java
@@ -0,0 +1,38 @@
+package Algorithms.leecode;
+
+/**
+ * 最长回文子字符串问题
+ * 反转字符串方法
+ *
+ */
+public class Solution5 {
+    public static void main(String[] args) {
+        String s = new Solution5().longestPalindrome("babad");
+    }
+    public String longestPalindrome(String s) {
+        if(s==null||s.equals(""))return s;
+        String reverse = new StringBuilder(s).reverse().toString();
+        int[][] arr=new int[s.length()][s.length()];
+        int maxlen=0;
+        int lastIndex=0;
+        for(int i=0;i<s.length();i++){
+            for(int j=0;j<s.length();j++){
+                if(s.charAt(i)==reverse.charAt(j)){
+                    if(i==0||j==0){
+                        arr[i][j]=1;
+                    }else {
+                        arr[i][j]=arr[i-1][j-1]+1;
+                    }
+                    if (arr[i][j] > maxlen) {
+                        int beforeRev = s.length() - 1 - j;
+                        if (beforeRev + arr[i][j] - 1 == i) { //判断下标是否对应
+                            maxlen = arr[i][j];
+                            lastIndex = i;
+                        }
+                    }
+                }
+            }
+        }
+        return s.substring(lastIndex-maxlen+1,lastIndex+1);
+    }
+}
