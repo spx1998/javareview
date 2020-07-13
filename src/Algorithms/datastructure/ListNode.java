bae8b5673bc59ace918e6928e073e154c706ad62
孙鹏翔 <sunpengxiang@sunpengxiangdeMacBook-Pro.local>
2020-07-13 22:59:17 +0800

    commit

--- src/Algorithms/datastructure/ListNode.java
+++
@@ -1,9 +0,0 @@
-package Algorithms.datastructure;
-public class ListNode {
-    public int val;
-    public ListNode next = null;
-    public ListNode(int val) {
-        this.val = val;
-    }
-}
-
--- src/experiment/PrintTest.java
+++
@@ -1,8 +0,0 @@
-package experiment;
-
-public class PrintTest {
-    public static void main(String[] args) {
-        System.out.println(3|9);//11  返回|运算的结果
-        System.out.println(3&9);//1  返回进行&运算的结果
-    }
-}
