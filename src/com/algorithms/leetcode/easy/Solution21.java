9a942bd1872c458ecddf6707750872d839c43cb9
sunpengxiang <457799096@qq.com>
2020-07-22 14:44:01 +0800

    commit

---
+++ src/com/algorithms/leetcode/easy/Solution21.java
@@ -0,0 +1,39 @@
+package com.algorithms.leetcode.easy;
+
+import com.algorithms.datastructure.ListNode;
+
+/**
+ * 将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
+ */
+public class Solution21 {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        } else if (l2 == null) {
+            return l1;
+        }
+        ListNode head = l1.val < l2.val ? l1 : l2;
+        ListNode temp = head;
+        if (head == l1) {
+            l1 = l1.next;
+        } else {
+            l2 = l2.next;
+        }
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                temp.next = l1;
+                l1 = l1.next;
+            } else {
+                temp.next = l2;
+                l2 = l2.next;
+            }
+            temp = temp.next;
+        }
+        if (l1 != null) {
+            temp.next = l1;
+        } else {
+            temp.next = l2;
+        }
+        return head;
+    }
+}
---
+++ src/com/algorithms/leetcode/medium/Solution22.java
@@ -0,0 +1,49 @@
+package com.algorithms.leetcode.medium;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 数字 n 代表生成括号的对数，请你设计一个函数，用于能够生成所有可能的并且 有效的 括号组合。
+ * 示例：
+ * 输入：n = 3
+ * 输出：[
+ * "((()))",
+ * "(()())",
+ * "(())()",
+ * "()(())",
+ * "()()()"
+ * ]
+ * 我的解法：
+ * 回溯法
+ */
+public class Solution22 {
+    public static void main(String[] args) {
+        new Solution22().generateParenthesis(3).forEach(System.out::println);
+    }
+
+    public List<String> generateParenthesis(int n) {
+        List<String> list = new ArrayList<>();
+        StringBuilder sb = new StringBuilder("(");
+        pushBracket(list, sb, 1, n - 1, n);
+        return list;
+    }
+
+    private void pushBracket(List<String> list, StringBuilder sb, int cur, int l, int r) {
+        if (l == 0 && r == 0) {
+            if (cur == 0) {
+                list.add(sb.toString());
+            }
+        }else {
+            if (l > 0) {
+                pushBracket(list, new StringBuilder(sb).append('('), cur + 1, l - 1, r);
+            }
+            if (r > 0) {
+                if (cur <= 0) {
+                    return;
+                }
+                pushBracket(list, new StringBuilder(sb).append(')'), cur - 1, l, r - 1);
+            }
+        }
+    }
+}
