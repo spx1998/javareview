69ef0322a836d094599dd83d41ead41dcfeb4ee6
sunpengxiang <457799096@qq.com>
2020-08-09 17:25:44 +0800

    commit

---
+++ src/com/algorithms/leetcode/medium/Solution153.java
@@ -0,0 +1,45 @@
+package com.algorithms.leetcode.medium;
+
+/**
+ * 假设按照升序排序的数组在预先未知的某个点上进行了旋转。
+ * ( 例如，数组 [0,1,2,4,5,6,7] 可能变为 [4,5,6,7,0,1,2] )。
+ * 请找出其中最小的元素。
+ * 你可以假设数组中不存在重复元素。
+ * 我的解法：二分法
+ * temp处的值大于pre处的值，则最小值在temp右侧，反之在左侧。
+ * 每次验证temp-1 ，temp ，temp+1的大小关系，若存在后者小于前者，则后者即最小值。
+ * 需要注意的是边界情况，例如[1,2][2,1]等数组，容易进入死循环，为了避免第一🀄️情况，需要判断数组是否进行了旋转，若没有，则直接返回nums[0].
+ *
+ * 相关问题 leetcode第33题 {@link Solution33}
+ */
+public class Solution153 {
+    public static void main(String[] args) {
+        System.out.println(new Solution153().findMin(new int[]{4, 5, 6, 7, 0, 1, 2}));
+    }
+
+    public int findMin(int[] nums) {
+        if (nums.length == 1) {
+            return nums[0];
+        }
+        int pre = 0;
+        int tail = nums.length - 1;
+        if (nums[tail] > nums[0]) {
+            return nums[0];
+        }
+        while (pre < tail) {
+            int temp = pre + (tail - pre) / 2;
+            if (temp != 0 && nums[temp] < nums[temp - 1]) {
+                return nums[temp];
+            }
+            if (temp != nums.length - 1 && nums[temp] > nums[temp + 1]) {
+                return nums[temp + 1];
+            }
+            if (nums[temp] < nums[pre]) {
+                tail = temp - 1;
+            } else {
+                pre = temp + 1;
+            }
+        }
+        return nums[0];
+    }
+}
---
+++ src/com/algorithms/leetcode/medium/Solution33.java
@@ -0,0 +1,74 @@
+package com.algorithms.leetcode.medium;
+
+import java.util.Arrays;
+
+/**
+ * 假设按照升序排序的数组在预先未知的某个点上进行了旋转。
+ * ( 例如，数组 [0,1,2,4,5,6,7] 可能变为 [4,5,6,7,0,1,2] )。
+ * 搜索一个给定的目标值，如果数组中存在这个目标值，则返回它的索引，否则返回 -1 。
+ * 你可以假设数组中不存在重复的元素。
+ * 你的算法时间复杂度必须是 O(log n) 级别。
+ * 我的解法：先找到旋转的位置，然后将数组还原，再二分查找。寻找的方式也类似二分查找。
+ * <p>
+ * 相关问题：
+ * leetcode 第153题 {@link Solution153}
+ */
+public class Solution33 {
+    public static void main(String[] args) {
+        System.out.println(new Solution33().search(new int[]{4, 5, 6, 7, 0, 1, 2}, 0));
+//        System.out.println(new Solution33().search(new int[]{5, 1, 3}, 0));
+//        System.out.println(new Solution33().search(new int[]{1, 3}, 0));
+
+    }
+
+    public int search(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+        if (nums.length == 1) {
+            return nums[0] == target ? 0 : -1;
+        }
+        int pre = 0;
+        int tail = nums.length - 1;
+        int cur = -1;
+        if (nums[tail] < nums[0]) {
+            while (pre < tail) {
+                int temp = pre + (tail - pre) / 2;
+                if (temp != 0 && nums[temp] < nums[temp - 1]) {
+                    cur = temp;
+                    break;
+                }
+                if (temp != nums.length - 1 && nums[temp] > nums[temp + 1]) {
+                    cur = temp + 1;
+                    break;
+                }
+                if (nums[temp] < nums[pre]) {
+                    tail = temp - 1;
+                } else {
+                    pre = temp + 1;
+                }
+            }
+        }
+        if (cur != -1) {
+            int[] newArray = new int[nums.length];
+            System.arraycopy(nums, cur, newArray, 0, nums.length - cur);
+            System.arraycopy(nums, 0, newArray, nums.length - cur, cur);
+            int newIndex = Arrays.binarySearch(newArray, target);
+            if (newIndex < 0) {
+                return -1;
+            }
+            if (newIndex < nums.length - cur) {
+                return newIndex + cur;
+            } else {
+                return newIndex - nums.length + cur;
+            }
+        } else {
+            int index = Arrays.binarySearch(nums, target);
+            if (index < 0) {
+                return -1;
+            } else {
+                return index;
+            }
+        }
+    }
+}
