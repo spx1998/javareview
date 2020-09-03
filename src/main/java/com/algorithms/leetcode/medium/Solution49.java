4029fc611ef615574f046f6a41ce20f5f4666c33
sunpengxiang <457799096@qq.com>
2020-09-03 12:54:09 +0800

    c

--- src/main/java/com/algorithms/leetcode/medium/Solution49.java
+++ src/main/java/com/algorithms/leetcode/medium/Solution49.java
@@ -1,2 +1,47 @@
-package com.algorithms.leetcode.medium;public class Solution49 {
+package com.algorithms.leetcode.medium;
+
+import java.util.*;
+
+/**
+ * 给定一个字符串数组，将字母异位词组合在一起。字母异位词指字母相同，但排列不同的字符串。
+ * 示例:
+ * 输入: ["eat", "tea", "tan", "ate", "nat", "bat"]
+ * 输出:
+ * [
+ * ["ate","eat","tea"],
+ * ["nat","tan"],
+ * ["bat"]
+ * ]
+ * 说明：
+ * 所有输入均为小写字母。
+ * 不考虑答案输出的顺序。
+ * <p>
+ * 我的解法：
+ * hashmap：字符串排序后的结果作为key，存异位词的数组作为list。
+ * 优化：
+ * 实现上的优化
+ */
+public class Solution49 {
+    public static void main(String[] args) {
+        new Solution49().groupAnagrams(new String[]{"eat", "tea", "tan", "ate", "nat", "bat"});
+    }
+
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        for (String s : strs) {
+            char[] chars = s.toCharArray();
+            Arrays.sort(chars);
+            String key = Arrays.toString(chars);
+            /*if (map.get(key) == null) {
+                map.put(key, new ArrayList<>(Collections.singleton(s)));
+            } else {
+                map.get(key).add(s);
+            }*/
+            if (!map.containsKey(key)) {
+                map.put(key, new ArrayList<>());
+            }
+            map.get(key).add(s);
+        }
+        return new ArrayList<>(map.values());
+    }
 }
