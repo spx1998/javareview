123d9594592e2dda053b559b2fc4088a28987f4e
sunpengxiang <457799096@qq.com>
2020-11-23 19:22:32 +0800

    c

--- src/main/java/com/multithreading/FutureTest.java
+++ src/main/java/com/multithreading/FutureTest.java
@@ -2,13 +2,38 @@ package com.multithreading;
 
 import java.util.concurrent.*;
 
+/**
+ * @author sunpengxiang
+ *
+ * future可以重复get。
+ */
 public class FutureTest {
-//    public static void main(String[] args) {
-//        ExecutorService executorService = new ThreadPoolExecutor(30, 50, 20,
-//                TimeUnit.SECONDS, new ArrayBlockingQueue<>(100));
-//        ;
-//        FutureTask<Integer> futureTask = executorService.submit();
-//    }
-//
-//    class
+    public static void main(String[] args) {
+        ExecutorService executorService = new ThreadPoolExecutor(30, 50, 20,
+                TimeUnit.SECONDS, new ArrayBlockingQueue<>(100));
+
+        Future<Integer> futureTask = executorService.submit(new TestTask());
+        try {
+            System.out.println("first " + futureTask.get(1, TimeUnit.SECONDS));
+        } catch (InterruptedException | ExecutionException | TimeoutException e) {
+            e.printStackTrace();
+        }
+        try {
+            System.out.println("second " + futureTask.get());
+            System.out.println("third " + futureTask.get());
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    static class TestTask implements Callable<Integer> {
+
+        @Override
+        public Integer call() throws Exception {
+            Thread.sleep(2000);
+            return 1;
+        }
+    }
 }
