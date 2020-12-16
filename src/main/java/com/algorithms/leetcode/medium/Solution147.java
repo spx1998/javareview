3d42af74e3ee4010887d5f87d1ca24f4a36700e4
sunpengxiang <457799096@qq.com>
2020-12-16 09:31:32 +0800

    c

---
+++ src/main/java/com/algorithms/leetcode/medium/Solution147.java
@@ -0,0 +1,52 @@
+package com.algorithms.leetcode.medium;
+
+import com.algorithms.datastructure.ListNode;
+
+/**
+ * 对链表进行插入排序。
+ * 插入排序的动画演示如上。从第一个元素开始，该链表可以被认为已经部分排序（用黑色表示）。
+ * 每次迭代时，从输入数据中移除一个元素（用红色表示），并原地将其插入到已排好序的链表中。
+ * 插入排序算法：
+ * 插入排序是迭代的，每次只移动一个元素，直到所有元素可以形成一个有序的输出列表。
+ * 每次迭代中，插入排序只从输入数据中移除一个待排序的元素，找到它在序列中适当的位置，并将其插入。
+ * 重复直到所有输入数据插入完为止。
+ * 示例 1：
+ * 输入: 4->2->1->3
+ * 输出: 1->2->3->4
+ * 示例2：
+ * 输入: -1->5->3->4->0
+ * 输出: -1->0->3->4->5
+ */
+public class Solution147 {
+    public static void main(String[] args) {
+        ListNode listNode = new ListNode(4);
+        listNode.next = new ListNode(2);
+        listNode.next.next = new ListNode(1);
+        listNode.next.next.next = new ListNode(3);
+        new Solution147().insertionSortList(listNode);
+    }
+
+    public ListNode insertionSortList(ListNode head) {
+        ListNode pre = new ListNode(-1);
+        pre.next = head;
+        ListNode prePoint = pre;
+        while (head != null) {
+            ListNode node1 = head.next;
+            ListNode temp = pre;
+            while (temp.next != head) {
+                if (temp.next.val > head.val) {
+                    ListNode node2 = temp.next;
+                    temp.next = head;
+                    head.next = node2;
+                    head = prePoint;
+                    break;
+                }
+                temp = temp.next;
+            }
+            prePoint = head;
+            head.next = node1;
+            head = head.next;
+        }
+        return pre.next;
+    }
+}
