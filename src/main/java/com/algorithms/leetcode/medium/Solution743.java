781f261ae6a23147df1abfa608de314fb01c65cd
sunpengxiang <457799096@qq.com>
2021-08-08 14:46:33 +0800

    最长最短路径

--- src/main/java/com/algorithms/leetcode/medium/Solution743.java
+++ src/main/java/com/algorithms/leetcode/medium/Solution743.java
@@ -1,5 +1,7 @@
 package com.algorithms.leetcode.medium;
 
+import java.util.*;
+
 /**
  * 有 n 个网络节点，标记为1到 n。
  * 给你一个列表times，表示信号经过 有向 边的传递时间。
@@ -9,7 +11,46 @@ package com.algorithms.leetcode.medium;
  */
 public class Solution743 {
     public int networkDelayTime(int[][] times, int n, int k) {
-//        TODO
-        return 0;
+        int[] minTime = new int[n];
+        for (int i = 0; i < n; i++) {
+            minTime[i] = Integer.MAX_VALUE;
+        }
+        Map<Integer, Integer> map = new HashMap<>();
+        boolean[] visited = new boolean[n];
+        visited[k - 1] = true;
+        minTime[k - 1] = 0;
+        for (int[] time : times) {
+            if (time[0] == k) {
+                map.put(time[1], time[2]);
+            }
+        }
+        while (!map.isEmpty()) {
+            int toAdd = -1;
+            Integer num = Integer.MAX_VALUE;
+            for (int key : map.keySet()) {
+                if (map.get(key) < num) {
+                    num = map.get(key);
+                    toAdd = key;
+                }
+            }
+            map.remove(toAdd);
+            minTime[toAdd - 1] = num;
+            visited[toAdd - 1] = true;
+            for (int[] time : times) {
+                if (time[0] == toAdd && !visited[time[1] - 1]) {
+                    if (map.get(time[1]) == null || time[2] + num < map.get(time[1])) {
+                        map.put(time[1], time[2] + num);
+                    }
+                }
+            }
+        }
+        int max = -1;
+        for (int min : minTime) {
+            if (min == Integer.MAX_VALUE) {
+                return -1;
+            }
+            max = Math.max(max, min);
+        }
+        return max;
     }
 }
